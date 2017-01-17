6a7b173fff3c614e90f240d8c10825ebc84138a1
mika watanabe <watanabe-mika@av.avex.co.jp>
2017-01-17 10:26:30 +0000

    work004

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_action :authenticate_user!
+  
   # deviseのコントローラを実行する時は「configure_permitted_parameters」メソッドを実行する
 #--**************************** 下記を追加 ****************************
   before_action :configure_permitted_parameters, if: :devise_controller?
